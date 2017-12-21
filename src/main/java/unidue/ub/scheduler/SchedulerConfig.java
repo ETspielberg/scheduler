b8cd59f57d2a0ea01c2fbfa20ce45786334050f8
Eike Spielberg <Eike.Spielberg@uni-due.de>
2017-12-21 09:39:32 +0100

    initial commit

---
+++ src/main/java/unidue/ub/scheduler/SchedulerConfig.java
@@ -0,0 +1,99 @@
+package unidue.ub.scheduler;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.Resources;
+import org.springframework.hateoas.client.Traverson;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import unidue.ub.settings.fachref.Profile;
+import unidue.ub.settings.fachref.Stockcontrol;
+import unidue.ub.settings.fachref.Sushiprovider;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Configuration
+@EnableScheduling
+public class SchedulerConfig {
+
+    private final static long dayInMillis = 24L*60L*60L*1000L;
+
+    private final static long HalfAYear = 182L * dayInMillis;
+
+    @Value("${ub.statistics.batch.url}")
+    String batchUrl;
+
+    @Value("${ub.statistics.runservice.url}")
+    String runserviceUrl;
+
+    @Value("${ub.statistics.settings.url}")
+    String settingsUrl;
+
+    @Scheduled(cron="0 0 1 * * ? *")
+    public void updateNotations() throws IOException {
+        callRestService(runserviceUrl + "/run/notationbuilder");
+    }
+
+    @Scheduled(cron="0 0 2 * * ? *")
+    public void updateNrequests() throws IOException {
+        callBatchJob("nrequests","");
+    }
+
+    @Scheduled(cron="0 23 * * SAT")
+    public void runEventanalyzer() throws IOException, URISyntaxException {
+        List<Stockcontrol> stockcontrols = (List<Stockcontrol>) getAllActiveStockcontrol(HalfAYear);
+        for (Stockcontrol stockcontrol : stockcontrols) {
+            callBatchJob("eventanalyzer",stockcontrol.getIdentifier());
+        }
+    }
+
+    private List<? extends Profile> getAllActiveStockcontrol(long interval) throws URISyntaxException {
+        Traverson traverson = new Traverson(new URI(settingsUrl + "/stockcontrol"), MediaTypes.HAL_JSON);
+        Traverson.TraversalBuilder tb = traverson.follow("$._links.self.href");
+        ParameterizedTypeReference<Resources<Stockcontrol>> typeRefDevices = new ParameterizedTypeReference<Resources<Stockcontrol>>() {};
+        Resources<Stockcontrol> resUsers = tb.toObject(typeRefDevices);
+        Collection<Stockcontrol> foundNotations = resUsers.getContent();
+        List<Stockcontrol> toBeExecuted = new ArrayList<>();
+        for (Stockcontrol stockcontrol : foundNotations) {
+            if (stockcontrol.getLastrun().getTime() < System.currentTimeMillis()- interval) {
+                toBeExecuted.add(stockcontrol);
+            }
+        }
+        return toBeExecuted;
+    }
+
+    @Scheduled(cron="0 0 1 15 * ?")
+    public void collectSushi() throws URISyntaxException, IOException {
+        Traverson traverson = new Traverson(new URI(settingsUrl + "/sushiprovider"), MediaTypes.HAL_JSON);
+        Traverson.TraversalBuilder tb = traverson.follow("$._links.self.href");
+        ParameterizedTypeReference<Resources<Sushiprovider>> typeRefDevices = new ParameterizedTypeReference<Resources<Sushiprovider>>() {};
+        Resources<Sushiprovider> resUsers = tb.toObject(typeRefDevices);
+        Collection<Sushiprovider> sushiproviders = resUsers.getContent();
+        for (Sushiprovider sushiprovider : sushiproviders) {
+            callBatchJob("sushi",sushiprovider.getIdentifier());
+        }
+    }
+
+    private int callBatchJob(String service, String identifier) throws IOException {
+        HttpClient client = new HttpClient();
+        GetMethod get = new GetMethod(batchUrl + "/run/" + service + "?identifier=" + identifier);
+        return client.executeMethod(get);
+    }
+    
+    private int callRestService(String url) throws IOException {
+        HttpClient client = new HttpClient();
+        GetMethod get = new GetMethod(url);
+        return client.executeMethod(get);
+    }
+
+
+}
